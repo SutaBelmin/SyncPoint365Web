dda15da442b3df1aa164e8700646bd63303098e0
DESKTOP-2GMKBRE\User <belmin.suta@edu.fit.ba>
2024-11-20 09:23:01 +0100

    Adding new component for search.

--- src/pages/cities/CitiesList.jsx
+++ src/pages/cities/CitiesList.jsx
@@ -1,13 +1,13 @@
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { citiesService, countriesService } from "../../services";
 import DataTable from "react-data-table-component";
 import { BaseModal, DeleteConfirmationModal } from "../../components/modal";
 import { useModal } from "../../context/ModalProvider";
 import { CitiesAdd, CitiesEdit } from "../cities";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { toast } from "react-toastify";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
-import Select from "react-select";
+import CitiesSearch from "./search/CitiesSearch";
 
 export const CitiesList = () => {
     const [data, setData] = useState([]);
@@ -15,21 +15,25 @@ export const CitiesList = () => {
     const [page, setPage] = useState(1);
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [totalItemCount, setTotalItemCount] = useState(0);
-    const [searchTerm, setSearchTerm] = useState("");
     const [countries, setCountries] = useState([]);
-    const [selectedCountryId, setSelectedCountryId] = useState(null);
+    const [searchParams, setSearchParams] = useState({ searchQuery: "", countryId: null });
 
-    const fetchData = useCallback(async (searchQuery = "", countryId = null) => {
+    const fetchData = useCallback(async () => {
         try {
-            const response = await citiesService.getPagedCities(countryId, searchQuery, page, rowsPerPage, null);
+            const response = await citiesService.getPagedCities(
+                searchParams.countryId,
+                searchParams.searchQuery,
+                page,
+                rowsPerPage,
+                null
+            );
             setData(response.data.items);
             setTotalItemCount(response.data.totalItemCount);
-
         } catch (error) {
             toast.error("There was an error. Please contact administrator.");
         }
-    }, [page, rowsPerPage]);
-
+    }, [searchParams, page, rowsPerPage]);
+    
     const fetchCountries = useCallback(async () => {
         try {
             const response = await countriesService.getList();
@@ -43,10 +47,10 @@ export const CitiesList = () => {
         }
     }, []);
 
-    useEffect(() => {
+    useEffect(() => { 
         fetchCountries();
-        fetchData(searchTerm, selectedCountryId?.value);
-    }, [fetchCountries, fetchData, searchTerm, selectedCountryId]);
+        fetchData();
+    }, [fetchCountries, fetchData]);
 
     const columns = [
         {
@@ -122,57 +126,34 @@ export const CitiesList = () => {
         setPage(1);
     };
 
-
-    const handleSearch = (value) => {
-        setSearchTerm(value);  
-        setPage(1);  
+    const onSearch = (params) => {
+        setSearchParams(params);
+        setPage(1);
     };
 
-    const handleCountryChange = (selectedOption) => {
-        setSelectedCountryId(selectedOption);
-        setPage(1);
-    }
-
     const clearFilters = () => {
-        setSearchTerm("");
-        setSelectedCountryId(null);
+        setSearchParams({ searchQuery: "", countryId: null });
         setPage(1);
-        fetchData();
-    }
+    };
 
     return (
         <div>
             <div className="flex justify-between items-center mb-4">
-                <div className="flex items-center space-x-4 mb-4">
-                    <input
-                        type="text"
-                        placeholder="Search by City"
-                        className="input-search h-10 rounded-md border-gray-300 w-[25rem]"
-                        value={searchTerm}
-                        onChange={(e) => handleSearch(e.target.value)}
-                    />
-
-                    <Select
-                        value={selectedCountryId}
-                        onChange={handleCountryChange}
-                        options={countries}
-                        placeholder="Select Country"
-                        isClearable
-                        className="h-10 border-gray-300 input-select-border w-[25rem]"
-                    />
-
-                    <button
-                        type="button"
-                        onClick={clearFilters}
-                        className="btn-clear h-10 bg-gray-700 text-white hover:bg-gray-300 hover:text-gray-900 py-2 px-4 rounded-md border border-gray-300 font-bold text-sm">
-                        Clear Filters
-                    </button>
-                </div>
+                <CitiesSearch
+                    onSearch={onSearch}
+                    onClearFilters={clearFilters}
+                    countries={countries}
+                    initialSearchTerm={searchParams.searchQuery}
+                    initialSelectedCountry={countries.find(
+                        (country) => country.value === searchParams.countryId
+                    )}
+                />
 
                 <button
-                    type='button'
+                    type="button"
                     onClick={onAddCitiesClick}
-                    className="btn-new h-10">
+                    className="btn-new h-10"
+                >
                     Add City
                 </button>
             </div>
@@ -187,11 +168,13 @@ export const CitiesList = () => {
                 paginationTotalRows={totalItemCount}
                 onChangePage={handlePageChange}
                 paginationPerPage={rowsPerPage}
-                onChangeRowsPerPage={(newRowsPerPage) => handleRowsPerPage(newRowsPerPage)}
+                onChangeRowsPerPage={(newRowsPerPage) =>
+                    handleRowsPerPage(newRowsPerPage)
+                }
                 highlightOnHover
                 persistTableHead={true}
-                noDataComponent="No cities available" />
-
+                noDataComponent="No cities available"
+            />
         </div>
     );
-}
+};
