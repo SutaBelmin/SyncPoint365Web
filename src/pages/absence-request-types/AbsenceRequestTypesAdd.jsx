0fef32a85185a14d97d237d3cf034e7f64bb264d
DESKTOP-2GMKBRE\User <belmin.suta@edu.fit.ba>
2024-11-28 15:47:13 +0100

    Modifying translation file.

--- src/pages/absence-request-types/AbsenceRequestTypesAdd.jsx
+++ src/pages/absence-request-types/AbsenceRequestTypesAdd.jsx
@@ -48,7 +48,7 @@ export const AbsenceRequestTypesAdd = ({ closeModal, fetchData }) => {
                             type="text"
                             id="name"
                             name="name"
-                            placeholder={t('ENTER_ABSENCE')}
+                            placeholder={t('ENTER_ABSENCE_REQUEST_TYPE_NAME')}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                         />
                         <ErrorMessage name="name" component="div" className="text-red-500 text-sm" />
