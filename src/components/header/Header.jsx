8d1809bfddc007148a3ad32f1010072025d637be
DESKTOP-2GMKBRE\User <belmin.suta@edu.fit.ba>
2025-01-24 16:33:53 +0100

    Adding a picture and name for the logged in user.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
@@ -6,16 +6,41 @@ import LanguageSwitcher from '../localization';
 import { useTranslation } from 'react-i18next';
 import './Header.css';
 import { useAuth } from '../../context/AuthProvider';
+import { toast } from "react-toastify";
+import { usersService } from '../../services';
+import defaultUserImage from '../../assets/images/defaultUser.PNG';
 
 const Header = ({ isCollapsed }) => {
     const { t } = useTranslation();
     const navigate = useNavigate();
     const { removeUser } = useAuth();
+    const { loggedUser } = useAuth();
+    const [profilePicture, setProfilePicture] = useState(null);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     const handleLogout = () => {
         removeUser();
         navigate('/');
     }
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const fetchUserImage = useCallback(async () => {
+        try {
+            const user = await usersService.getById(loggedUser.id);
+            const userImage = user.data.imageContent ? `data:image/jpeg;base64,${user.data.imageContent}` : defaultUserImage;
+            setProfilePicture(userImage);
+        } catch (error) {
+            toast.error(t('ERROR_LOADING_USER'));
+        }
+    }, [loggedUser.id, t]);
+
+    useEffect(() => {
+        if (loggedUser)
+            fetchUserImage();
+    }, [loggedUser, fetchUserImage]);
 
     return (
         <header className={`header-comp ${isCollapsed ? 'header-collapsed' : ''}`}>
@@ -24,14 +49,49 @@ const Header = ({ isCollapsed }) => {
                     <span className="text-custom-blue">SyncPoint</span>
                     <span className="text-yellow-600">365</span>
                 </h1>
-                <LanguageSwitcher />
-                <button
-                    onClick={handleLogout}
-                    className="flex items-center text-sm hover:text-gray-500"
-                >
-                    <FontAwesomeIcon icon={faSignOutAlt} className="mr-2 text-black" />
-                    <span className="text-black">{t('LOG_OUT')}</span>
-                </button>
+
+                <div className="mr-3">
+                    <LanguageSwitcher />
+                </div>
+
+                <div className="flex items-center gap-2 relative mr-8 ">
+                    <div
+                        onClick={toggleDropdown}
+                        className="cursor-pointer flex items-center gap-2 relative"
+                    >
+                        {profilePicture && (
+                            <div className="w-9 h-9 rounded-full overflow-hidden border-2 border-gray-300">
+                                <img
+                                    src={profilePicture}
+                                    alt="Profile"
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
+                        )}
+                        <span className='hide-for-small'>{loggedUser.firstName}</span>
+                    </div>
+
+                    {isDropdownOpen && (
+                        <div
+                            className="absolute top-[calc(100%+8px)] left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-lg shadow-lg w-32 z-10"
+                        >
+                            <div className="absolute -top-2 left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white border-l border-t border-gray-300 rotate-45"></div>
+                            <ul className="flex flex-col text-sm">
+                                <li
+                                    onClick={handleLogout}
+                                    className="py-2 px-4 hover:bg-gray-100 cursor-pointer text-black"
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faSignOutAlt}
+                                        className="mr-2 text-black"
+                                    />
+                                    {t('LOG_OUT')}
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
+
             </div>
         </header>
     );
